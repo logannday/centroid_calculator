c2fd31a4481a709e22aeeb7a055bed7506373aca
logannday <logannday@gmail.com>
2025-05-06 10:10:47 -0700

    interface residues calculated

--- centroid.py
+++ centroid.py
@@ -1,85 +1,37 @@
-import os, argparse, re
-from Bio.PDB.PDBParser import PDBParser
-from Bio.PDB.Chain import Chain
-from Bio.PDB.Structure import Structure
+from typing import List
+# from Bio.PDB.PDBParser import PDBParser
+# from Bio.PDB.Chain import Chain
+# from Bio.PDB.Structure import Structure
+from Bio.PDB.Residue import Residue
 from Bio.PDB.internal_coords import *
-from Bio.PDB.PICIO import write_PIC, read_PIC, read_PIC_seq
-from Bio.PDB.ic_rebuild import write_PDB, IC_duplicate, structure_rebuild_test
-from Bio.PDB.SCADIO import write_SCAD
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.PDB.PDBIO import PDBIO
+# from Bio.PDB.PICIO import write_PIC, read_PIC, read_PIC_seq
+# from Bio.PDB.ic_rebuild import write_PDB, IC_duplicate, structure_rebuild_test
+# from Bio.PDB.SCADIO import write_SCAD
+# from Bio.Seq import Seq
+# from Bio.SeqRecord import SeqRecord
+# from Bio.PDB.PDBIO import PDBIO
 import numpy as np
 
-class Target:
-    def __init__(self, chain, residues):
-        self.chain = chain
-        self.residues = residues
-
-def calculate_centroid(structure, targets):
-    model = structure[0]
-
+def calculate_centroid(residues: List[Residue]):
     xs = []
     ys = []
     zs = []
-    for target in targets:
-        chain = model[target.chain]
-        for i in target.residues:
-            coords = chain[i]["CA"].get_coord()
-            xs.append(coords[0])
-            ys.append(coords[1])
-            zs.append(coords[2])
 
+    for res in residues:
+        coords = res["CA"].get_coord()
+        xs.append(coords[0])
+        ys.append(coords[1])
+        zs.append(coords[2])
 
     centroid = [np.average(xs), np.average(ys), np.average(zs)]
 
     return centroid
 
-def closest_interface_residue(structure, targets, res):
-    model = structure[0]
-    
-    alpha_carbon = model[res.chain][res.residues[0]]["CA"] # alpha carbon of residue of interest
+def closest_interface_residue(interface_residues, target_res):
     min_distance = np.inf
 
     # Finding the closest interface residue to the residue of interest
-    for target in targets: 
-        chain = model[target.chain]
-        for i in target.residues:
-            for atom in chain[i]:
-                min_distance = np.minimum(min_distance, atom - alpha_carbon)
+    for i_res in interface_residues:
+            min_distance = np.minimum(min_distance, i_res["CA"] - res["CA"])
 
     return min_distance
-    
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('pdb-id', type=str)
-    parser.add_argument('pdb-filepath', type=str, help = "filepath to pdb. + ex: \"./1ak4.pdb\"")
-    parser.add_argument('--targets', dest='targets', type=str, default="A", required=True, 
-                        help = "String defining the interface residues. Example:" +
-                        "A1,12,13,15B14,15 indicates chain A residues 1, 12, 13, 14" +
-                        "and chain B residues 14 and 15")
-
-    pattern = r'([A-Za-z])((?:\d+,?)*)'
-    targets = "A1,12,13,15B14,15"
-    matches = re.findall(pattern, targets)
-
-    target_objects = []
-    for chain_id, residues_str in matches:
-        residues = [int(r) for r in residues_str.split(',') if r]
-        target_objects.append(Target(chain_id, residues))
-
-    for obj in target_objects:
-        print(obj.chain, obj.residues)
-
-    # load a structure as normal, get first chain
-    parser = PDBParser()
-    myProtein = parser.get_structure("1ak4", "1ak4.pdb")
-    # myChain = myProtein[0]["A"]
-    centroid = calculate_centroid(myProtein, target_objects)
-    min_distance = closest_interface_residue(myProtein, target_objects, Target("A", [25]))
-    print("Centroid: ", centroid)
-    print("Min distance:", min_distance)
-
-
-if __name__ == "__main__":
-    main()
--- data_grabber.py
+++ data_grabber.py
@@ -10,8 +10,8 @@ from Bio.PDB.internal_coords import *
 # from Bio.SeqRecord import SeqRecord
 # from Bio.PDB.PDBIO import PDBIO
 import pandas as pd
-# import numpy as np
-from centroid import Target, calculate_centroid, closest_interface_residue
+from centroid import calculate_centroid, closest_interface_residue
+from detect_interface import get_interface_residues
 from dataclasses import dataclass
 
 @dataclass
@@ -23,7 +23,7 @@ class mutant:
 
 def get_structures(path):
     parser = PDBParser()
-    myProtein = parser.get_structure("1ak4", "1ak4.pdb")
+    myProtein = parser.get_structure("1brs", "1brs.pdb")
     path = "./1brs_PDBS/"
     filenames = os.listdir(path)
     mutants = []
@@ -43,21 +43,37 @@ def get_structures(path):
         mutants.append(mutant(struct, True, residue, chain))
     return mutants
 
-def get_data(mutants):
-    target = Target("A", [5, 6, 7, 9, 10])
-    targets = [target]
+# Get the residue defined at the absolute location in the pdb
+def get_res_by_absolute_index(struc, location):
+    chain_a_len = len(list(struc[0]['A'].get_residues()))
+    if chain_a_len > location:
+        return struc[0]['A'][location]
+    return struc[0]['B'][location - chain_a_len]
+
+def populate_dataframe(wildtype: Structure, mutants):
     rows = []
     for mutant in mutants:
-        centroid = calculate_centroid(mutant.structure, targets)
-        print(mutant)
+        interface_residues = get_interface_residues(mutant, ["C", "F"])
+        centroid = calculate_centroid(interface_residues)
+        inserted_residue = get_res_by_absolute_index(mutant.structure, mutant.location)
+        cir = closest_interface_residue(interface_residues, inserted_residue)
+        print(inserted_residue.get_coord())
+        breakpoint()
         rows.append({ 
-            "chain": mutant.chain,
+            "location": mutant.location,
+            "residue": mutant.residue,
         })
     df = pd.DataFrame.from_dict(rows)
+    return df
 
 def main():
+    parser = PDBParser()
+    wildtype = parser.get_structure("1brs", "./1brs.pdb")
+    for chain in wildtype[0]:
+        print(chain.id)
     mutants = get_structures("./1brs_PDBS")
-    data = get_data(mutants)
+    data = populate_dataframe(wildtype, mutants)
+    # print(data.head())
 
 if __name__ == "__main__":
     main()
